98c468eefef1dbd67a3c3569b26136fc9961a36a
Nimra Arif <nimra.arif@tintash.com>
2019-07-01 18:12:12 +0500

    An error with dynamic sass loader loading

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,6 @@
+// import { parseInputs } from "./utils/parse-inputs";
 
 export const run=(AlertService)=>{
     alertService.hideErrors();
   };
   
-
-
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
- alert("HELLO FROM WEB APP")
- import {run } from  "./app/app";
-
- const alertservice= new AlertService();
- run(alertservice)
+// alert("HELLO FROM WEB APP")
+import {run } from  "./app/app";
+import {AlertService} from "./app/AlertService"
+import "./main.scss";
+const alertservice= new AlertService();
+run(alertservice);
