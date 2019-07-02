b1c75c590ecfd87e705d463213575819c31156d7
Nimra Arif <nimra.arif@tintash.com>
2019-07-02 10:14:43 +0500

    Final Version

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
-const path=require("path");
-module.exports={
-    mode:"development",   
-    entry:"./src/index.js" ,
-    output:{
+
+const path = require("path");
+const webpack = require("webpack");
+const webpack_rules = [];
+const webpackOption = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
         filename: "main.js",
-        path:path.resolve(__dirname,"dist")
-},
-module:{
-    rules:[
-        {
-          test:/\.css$/,
-          use:["style-loader", "css-loader"]
-        }
-    ]
-}
-
-
+    },
+    module: {
+        rules:[
+            {
+              test:/\.scss$/,
+              use:["style-loader", "css-loader","sass-loader"]
+            }
+            ]
+    }
 };
- 
+let babelLoader = {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+        loader: "babel-loader",
+        options: {
+            presets: ["@babel/preset-env"]
+        }
+    }
+};
+webpack_rules.push(babelLoader);
+module.exports = webpackOption;
